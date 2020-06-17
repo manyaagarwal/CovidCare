14f92fed9976f8e33fad83d0a53a0a5eb6650360
agarwalmanya <manya18ag@gmail.com>
2020-06-18 00:55:36 +0530

    add theme colors

---
+++ covid_care/lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Map <int, Color> accentColor = {
+  50:  Color.fromRGBO(67, 220, 190, .1),
+  100: Color.fromRGBO(67, 220, 190, .2),
+  200: Color.fromRGBO(67, 220, 190, .3),
+  300: Color.fromRGBO(67, 220, 190, .4),
+  400: Color.fromRGBO(67, 220, 190, .5),
+  500: Color.fromRGBO(67, 220, 190, .6),
+  600: Color.fromRGBO(67, 220, 190, .7),
+  700: Color.fromRGBO(67, 220, 190, .8),
+  800: Color.fromRGBO(67, 220, 190, .9),
+  900: Color.fromRGBO(67, 220, 190, 1),
+};
+
+
+Map <int, Color> primaryColor = {
+  50:  Color.fromRGBO(148, 171, 249, .1),
+  100: Color.fromRGBO(148, 171, 249, .2),
+  200: Color.fromRGBO(148, 171, 249, .3),
+  300: Color.fromRGBO(148, 171, 249, .4),
+  400: Color.fromRGBO(148, 171, 249, .5),
+  500: Color.fromRGBO(148, 171, 249, .6),
+  600: Color.fromRGBO(148, 171, 249, .7),
+  700: Color.fromRGBO(148, 171, 249, .8),
+  800: Color.fromRGBO(148, 171, 249, .9),
+  900: Color.fromRGBO(148, 171, 249, 1),
+};
--- covid_care/lib/main.dart
+++ covid_care/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:covidcare/home.dart';
 import 'package:flutter/material.dart';
 
-import 'Calendar_Questionaire.dart';
+import 'constants.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-         primarySwatch: Colors.blue,
+         primarySwatch: MaterialColor(0XFF94ABF9, primaryColor),
+        accentColor: MaterialColor(0XFF43DCBE, accentColor),
        ),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: HomePage(),
@@ -21,3 +25,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//rgb(67, 220, 190)
+//rgb(69, 198, 230)
+//rgb(148, 171, 249)
