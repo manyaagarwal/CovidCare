1ee169a9a097dbb6c624e668d33acf253c8c361f
agarwalmanya <manya18ag@gmail.com>
2020-06-18 13:52:10 +0530

    clean up chart.dart

--- covid_care/lib/chart.dart
+++ covid_care/lib/chart.dart
@@ -2,19 +2,6 @@ import 'package:covidcare/faq.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-//class LChart extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Example Dialogflow Flutter',
-//      theme: new ThemeData(
-//        primarySwatch: Colors.deepOrange,
-//      ),
-//      debugShowCheckedModeBanner: false,
-//      home: new LineChartSample1(),
-//    );
-//  }
-//}
 
 class LineChartSample1 extends StatefulWidget {
   @override
